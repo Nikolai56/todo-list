1584cdb20d6fdbe7adc3b9f8d383a360c3e03ca0
Nikolai56 <>
2019-01-17 01:49:35 +0300

    Updates

---
+++ precache-manifest.5a4af9cc5b78d2b915924e3d407707e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6f47f260b12c7ad9f72e",
+    "url": "/todo-list/static/js/runtime~main.6f47f260.js"
+  },
+  {
+    "revision": "e5c1dd22b6a6a196f326",
+    "url": "/todo-list/static/js/main.e5c1dd22.chunk.js"
+  },
+  {
+    "revision": "d958f79c2162dda9f115",
+    "url": "/todo-list/static/js/1.d958f79c.chunk.js"
+  },
+  {
+    "revision": "e5c1dd22b6a6a196f326",
+    "url": "/todo-list/static/css/main.342d1f11.chunk.css"
+  },
+  {
+    "revision": "59e6cff3c64c404c44935f0fa8ecfb9b",
+    "url": "/todo-list/index.html"
+  }
+];
